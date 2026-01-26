be40e19c6b91844151471831e346d2f7811ff604
Ryan Läuppi <pa33xxl@eduvaud.ch>
2026-01-26 21:07:28 +0100

    step 4-6

--- Exercice Gadget/src/main.js
+++ Exercice Gadget/src/main.js
@@ -4,20 +4,54 @@ const app = Vue.createApp({
       title: "Vue Gadget Shop",
       description:
         "Découvrez les derniers gadgets électroniques de haute technologie !",
-      gadget: {
-        phone: {
-          title: "Smartphone",
-          description: "smarphone stylé",
-          nom: "Smartphone XZ",
-          prix: "799€",
+      cart: [],
+      gadgets: [
+        {
+          id: 1,
+          name: "Smartphone XZ",
+          price: "799€",
           image: "./assets/phone.jpg",
           inStock: true,
         },
-      },
+        {
+          id: 2,
+          name: "Laptop Pro",
+          price: "1299€",
+          image: "./assets/laptop.jpg",
+          inStock: false,
+        },
+        {
+          id: 3,
+          name: "Ecouteurs Bluetooth",
+          price: "199€",
+          image: "./assets/earbuds.jpg",
+          inStock: true,
+        },
+      ],
     };
   },
   methods: {
-    inStock() {},
+    addToCart(gadget) {
+      this.cart.push(gadget);
+    },
+    removeFromCart(index) {
+      this.cart.splice(index, 1);
+    },
   },
-  computed: {},
+  computed: {
+    cartTotal() {
+      let total = 0;
+      for (let i = 0; i < this.cart.length; i++) {
+        total += Number(this.cart[i].price.replace("€", ""));
+      }
+      return total;
+    },
+    discountedTotal() {
+      if (this.cartTotal > 1000) {
+        return this.cartTotal * 0.9;
+      } else {
+        return this.cartTotal;
+      }
+    },
+  },
 });
